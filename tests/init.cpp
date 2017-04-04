57f00c48b4d7726f86ef5bd8026d411ed9d39265
SVolkoff <sabina-volkoff@yandex.com>
2017-04-04 13:08:17 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,4 +1,4 @@
-#include "tree.hpp"
+#include <tree.h>
 #include <catch.hpp>
 
 SCENARIO ("init", "[init]")
@@ -20,4 +20,28 @@ SCENARIO("find_node", "[init]")
   test.insert_node(4);
   REQUIRE(test.find_node(4, test.root_()) != NULL);
   REQUIRE(test.find_node(4, test.root_())->key == 4);
+}
+SCENARIO("get root", "[init]")
+{
+  Tree<int> test;
+  test.insert(4);
+  REQUIRE(test.root_() != 0);
+}
+SCENARIO ("reading", "[init]")
+{
+   Test<int> test;
+   test.reading("file1.txt");
+   REQUIRE(test.search_result(3) == 1);
+   REQUIRE(test.search_result(4) == 1);
+   REQUIRE(test.get_count() == 2);
+}
+
+SCENARIO ("writing", "[init]")
+{
+  Test<int> test1;
+  test1.insert(4);
+  test1.writing("file2.txt");
+  Test<int> test2;
+  test2.reading("file2.txt");
+  REQUIRE(test.get_count() == test2.get_count());
 }
